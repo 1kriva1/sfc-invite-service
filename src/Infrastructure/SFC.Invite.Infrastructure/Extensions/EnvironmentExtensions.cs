3ca6dfe42bb8045b5523f1526dd1d98db3270ebc
Andrii Kryvoruk <AndriiK@neogames.com>
2025-11-19 14:16:30 +0200

    Add docker 1

---
+++ src/Infrastructure/SFC.Invite.Infrastructure/Extensions/EnvironmentExtensions.cs
@@ -0,0 +1,7 @@
+﻿using SFC.Invite.Infrastructure.Constants;
+
+namespace SFC.Invite.Infrastructure.Extensions;
+public static class EnvironmentExtensions
+{
+    public static bool IsRunningInContainer => Environment.GetEnvironmentVariable(EnvironmentConstants.RunningInContainer) == "true";
+}
